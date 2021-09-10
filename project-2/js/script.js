d72d362cd12d504fbb0f30b6000ccadf01915f22
George <klimovgg@gmail.com>
2021-09-10 18:21:42 +0200

    add menu button and script for topening and closing of menu

--- project-2/js/script.js
+++ project-2/js/script.js
@@ -2,3 +2,23 @@
 const shareButton = document.querySelectorAll('.section__share-btn');
 
 shareButton.forEach(btn => btn.addEventListener("click", () => alert(window.location.href)))
+
+const menuContainer = document.querySelector(".header__nav"),
+  header = document.querySelector('.header'),
+  closeBtn = document.querySelector(".header__close"),
+  openBtn = document.querySelector(".header__open"),
+  menuOpen = () => {
+    openBtn.addEventListener("click", () => {
+      menuContainer.classList.add("active")
+      header.classList.add('active')
+    })
+  },
+  menuClose = () => {
+    closeBtn.addEventListener("click", () => {
+      menuContainer.classList.remove("active")
+      header.classList.remove('active')
+    })
+  };
+
+menuClose();
+menuOpen();
